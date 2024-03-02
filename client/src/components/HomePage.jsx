70cb19d16e3067e01753e7d71b79655c4797a91a
Amisha Mishra <amishamishra12886@gmail.com>
2024-03-02 22:15:34 +0530

    seventh-commit

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 export function HomePage() {
+    const navigate = useNavigate();
+    const transcriptOnClick = () => {
+        navigate('/transcripts');
+      };
+      const ContactOnClick = () => {
+        navigate('/contact-details');
+      };
     return (
         <div>
             <div style={{
                 alignItems:"center",
                 display:"flex",
                 justifyContent:"center",
-                fontSize:"25px"
+                fontSize:"25px",
+                color:"darkgoldenrod"
             }}>
             <h1>Aryabhatta</h1>
             </div>
@@ -18,33 +30,37 @@ export function HomePage() {
             }}>
 
                 <div style={{
-                    backgroundColor:"gray",
+                    backgroundColor:"pink",
                     color:"white",
                     width:"100px",
                     height:"40px",
-                    padding:"20px"
+                    padding:"20px",
+                    cursor:"pointer"
                 }}>Aryabhatta V1 Logo</div>
                 <div style={{
-                    backgroundColor:"gray",
+                    backgroundColor:"pink",
                     color:"white",
                     width:"100px",
                     height:"40px",
-                    padding:"20px"
+                    padding:"20px",
+                    cursor:"pointer"
                 }}>View your teachers (AI Agents)</div>
                 <div style={{
-                    backgroundColor:"gray",
+                    backgroundColor:"pink",
                     color:"white",
                     width:"100px",
                     height:"40px",
-                    padding:"20px"
-                }}>Call Transcripts</div>
+                    padding:"20px",
+                    cursor:"pointer"
+                }} onClick={transcriptOnClick}>Call Transcripts</div>
                 <div style={{
-                    backgroundColor:"gray",
+                    backgroundColor:"pink",
                     color:"white",
                     width:"100px",
                     height:"40px",
-                    padding:"20px"
-                }}>Contact Details</div>
+                    padding:"20px",
+                    cursor:"pointer"
+                }} onClick={ContactOnClick}>Contact Details</div>
             </div>
             <div style={{
                 marginTop:"80px",
@@ -54,15 +70,27 @@ export function HomePage() {
             <div style={{
                 display:"flex",
                 justifyContent:"center",
-                backgroundColor:"gray",
                 color:"white",
                 width:"130px",
                 height:"20px",
                 padding:"20px",
-                backgroundColor:"white",
-                color:"Black"
+                backgroundColor:"pink",
+                color:"White",
+                cursor:"pointer"
             }}>
                 LLM Playground
+            </div>
+            </div>
+            <div style={{
+                display:"flex",
+                justifyContent:"center"
+            }}>
+            <div style={{
+                backgroundColor:"lightskyblue",
+                height:"300px",
+                width:"800px",
+                marginTop:"20px"
+            }}>
             </div>
             </div>
         </div>
